c63d0948e21ba76a53d8cf4a68cb788c182c8035
Wellington Feitoza <spaaws@gmail.com>
2026-09-21 20:24:10 -0300

    feat: refactor /books endpoint to use a books array for consistency

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+const books = [
+    { id: 1, title: 'Harry Potter', author: 'J.K. Rowling' },
+    { id: 2, title: 'Senhor dos Anéis', author: 'J.R.R. Tolkiens' },
+    { id: 3, title: 'O Hobbit', author: 'J.R.R. Tolkiens' }
+  ]
+
 app.get('/', (req, res) => {
   res.send('API-BOOK está funcionando!')
 })
 
 app.get('/books', (req, res) => {
-  res.json([
-    { id: 1, title: 'Harry Potter', author: 'J.K. Rowling' },
-    { id: 2, title: 'Senhor dos Anéis', author: 'J.R.R. Tolkiens' },
-    { id: 3, title: 'O Hobbit', author: 'J.R.R. Tolkiens' }
-  ])
+  res.json(books)
+})
+
+app.get('/books/:id', (req, res) => {
+  const bookId = parseInt(req.params.id)
+  const book = books.find(b => b.id === bookId)
+  if (!book) {
+    return res.status(404).json({ message: 'Livro não encontrado' })
+  }
+  res.json(book)
 })
 
 app.listen(port, () => {
