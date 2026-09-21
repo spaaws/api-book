36fa33170bfb50c5854d266fc730cbca4fb7c5b7
Wellington Feitoza <spaaws@gmail.com>
2026-09-21 19:46:56 -0300

    feat: initialize Express.js API with basic routes and package.json

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World TURMA!')
+})
+
+// endpoint para a rota /turma
+app.get('/turma', (req, res) => {
+  res.send('Me acompanhem nesse teste TURMA!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
